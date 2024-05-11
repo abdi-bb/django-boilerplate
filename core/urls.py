c4563481b5e9475e67d137967d47d75e42150a69
Abdi Berhe <abdiberhe@gmail.com>
2024-05-11 14:37:34 +0300

    chore: Update DRF Spectacular settings for API documentation

--- core/urls.py
+++ core/urls.py
@@ -35,9 +35,12 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Documentation urls
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns += [
-    path("api/docs/", SpectacularAPIView.as_view(), name="docs"),
-    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    # YOUR PATTERNS
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
