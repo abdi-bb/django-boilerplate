1da0147d00d6b6034c82375c960842c4339616ce
Abdi Berhe <abdiberhe@gmail.com>
2024-05-13 10:29:27 +0300

    Update development.py

--- core/settings/development.py
+++ core/settings/development.py
@@ -7,4 +7,4 @@ CORS_ALLOW_ALL_ORIGINS = True
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # Media and Static files(you don't this for production, as you may use other services like AWS S3, Google Cloud Storage, etc.)
-MEDIA_ROOT = os.path.join(BASE_DIR, "../", "media_dev") # store user uploaded files for development
+# MEDIA_ROOT = os.path.join(BASE_DIR, "../", "media_dev") # store user uploaded files for development
